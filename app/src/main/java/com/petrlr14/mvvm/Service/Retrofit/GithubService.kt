40d2ff95f9c209adddaf4f3b1cae8a770470ddef
Alex7498-SV <00068817@uca.edu.sv>
2019-05-31 14:01:33 -0600

    retrofit

--- app/src/main/java/com/petrlr14/mvvm/Service/Retrofit/GithubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/Service/Retrofit/GithubService.kt
@@ -1,5 +1,14 @@
 package com.petrlr14.mvvm.Service.Retrofit
 
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface GithubService {
+
+    @GET("/users/{usuario}/repos")
+    fun getRepos(@Path("usuario") usuario:String) : Deferred<Response<List<GitHubRepo>>>
 
 }

4127f3414d08df8f6594664196da37f1b201e9c2
Alex7498-SV <00068817@uca.edu.sv>
2019-05-31 14:05:48 -0600

    archivo GithubService = NetworkUtils

--- app/src/main/java/com/petrlr14/mvvm/Service/Retrofit/GithubService.kt
+++ app/src/main/java/com/petrlr14/mvvm/Service/Retrofit/GithubService.kt
@@ -1,8 +1,11 @@
 package com.petrlr14.mvvm.Service.Retrofit
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.petrlr14.mvvm.database.entities.GitHubRepo
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -11,4 +14,11 @@ interface GithubService {
     @GET("/users/{usuario}/repos")
     fun getRepos(@Path("usuario") usuario:String) : Deferred<Response<List<GitHubRepo>>>
 
+    companion object {
+        fun getRetrofit() : GithubService{
+            return  Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build().create(GithubService::class.java)
+        }
+    }
 }
